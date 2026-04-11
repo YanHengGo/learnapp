f265f8df9c9a886f78496412b30ad341c6d97996
gen <genmanabu@gmail.com>
2026-04-11 12:05:43 +0900

    splash画面追加

---
+++ app/src/main/java/com/learn/app/SplashViewModel.kt
@@ -0,0 +1,33 @@
+package com.learn.app
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.learn.app.core.datastore.TokenDataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val tokenDataStore: TokenDataStore,
+) : ViewModel() {
+
+    sealed interface Destination {
+        object Auth : Destination
+        object Children : Destination
+    }
+
+    var destination by mutableStateOf<Destination?>(null)
+        private set
+
+    init {
+        viewModelScope.launch {
+            val token = tokenDataStore.token.first()
+            destination = if (token != null) Destination.Children else Destination.Auth
+        }
+    }
+}
